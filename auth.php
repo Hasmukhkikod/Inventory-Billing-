070f00e97df5af98f9cdef09bc04145872f0b622
Hasmukh <your-email@example.com>
2026-07-25 18:15:13 +0530

    chnages

---
+++ auth.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script>
+        if (window.location.hash === '#login') {
+            window.location.href = '/login';
+        } else {
+            window.location.href = '/demo/login';
+        }
+    </script>
+</head>
+<body>
+    Redirecting...
+</body>
+</html>
